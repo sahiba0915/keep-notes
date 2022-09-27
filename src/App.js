a34dd43148ad3ee8b15abe55c9992621cc6e13f6
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-09-27 19:28:35 +0530

    feat: added delete note functionality

--- src/App.js
+++ src/App.js
@@ -34,9 +34,15 @@ const addNote = (text) => {
   const newNotes = [...notes, newNote]
   setNotes(newNotes);
 }
+
+
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note) => note.id!== id);
+  setNotes(newNotes);
+}
   return (
     <div className='container'>
-      <NotesList notes={notes}  handleAddNote={addNote}/>
+      <NotesList notes={notes}  handleAddNote={addNote} handleDeleteNote={deleteNote}/>
     </div>  
     )
 }
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = ({id, text, date}) => {
+const Note = ({id, text, date, handleDeleteNote}) => {
   return (
     <div className='note'>
         <span>{text}</span>
         <div className='note-footer'>
             <span>{date}</span>
-            <MdDeleteForever className='delete-icon' size='1.3rem'/>       
+            <MdDeleteForever className='delete-icon' size='1.3rem' onClick={() => handleDeleteNote(id)}/>       
             </div>
     </div>
   )
